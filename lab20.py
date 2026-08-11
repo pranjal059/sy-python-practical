f246c628fe0b79b6fda09ee502d85e9e632e0e36
pranjal059 <pawarpranjal514@gmail.com>
2026-08-11 13:17:36 +0530

    Add files via upload

---
+++ lab20.py
@@ -0,0 +1,10 @@
+name=input("Enter customer name:")
+feedback=input("Enter feedback:")
+
+name=(name.title())
+feedback=(feedback.strip().rstrip())
+
+print("********customer feedback*******")
+
+print("Enter customer name:",name)
+print("Enter feedback:",feedback)
