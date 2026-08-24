c739e9c464398f0703dca6bed924ab4c652e9a6d
pranjal059 <pawarpranjal514@gmail.com>
2026-08-24 10:06:33 +0530

    Add files via upload

--- 23.py
+++ 23.py
@@ -1,62 +1,30 @@
-marks=[]
-
-while True:
-    print("\n---Student Marks Management System---")
-    print("1. Insert Marks")
-    print("2. Display Marks")
-    print("3. Update Marks")
-    print("4. Delete Marks")
-    print("5. Exit")
-
-    choice = int(input("Enter your choice:"))
-
-    #Insertion
-    if choice == 1:
-        mark = int(input("Enter student mark:"))
-        marks.append(mark)
-        print("Marks inserted succesfully.")
+name=input("Enter your name: ")
 
 
-    #Traversal
-    elif choice == 2:
-        if len(marks) == 0:
-            print("No marks available.")
+
+marks1=float(input("Enter your marks of subject1: "))
+
+marks2=float(input("Enter your marks of subject2: "))
+marks3=float(input("Enter your marks of subject3: "))
+
+total = marks1+marks2+marks3
+
+average=total / 3
 
 
-        else:
-            print("Student Marks:")
-            for i in range (len(marks)):
-                print("Student",i+1, ":", marks[i])
 
 
-    #Updating
 
-    elif choice == 3:
-        student = int(input("Enter student number to update:"))
-        if i <= student <= len(marks):
-            new_mark = int(input("Enter new marks:"))
-            marks[student-1] = new_mark
-            print("Marks updated succesfully.")
+print("======== Display score card ==========")
 
-        else:
-            print("Invalid student number.")
+print("subject1", marks1)
 
+print("subject2", marks2)
 
-    #Deletion 
-    elif choice == 4:
-        student = int(input("Enter student number to delete:"))
-        if 1<=student <= len(marks):
-            marks.pop(student-1)
-            print("Marks deleted succesfully.")
+print("subject3", marks3)
 
-        else:
-            print("Invalid student number.")
+print("Total marks", total)
 
-    #exit           
+print("Average", round (average))
 
-    elif choice == 5:
-        print("program ended.") 
-        break
-
-    else:
-        print("Invalid choice.")
+print("---------------")
