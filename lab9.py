7f31669e6128caaf74fa93935d486a510d28695f
pranjal059 <pawarpranjal514@gmail.com>
2026-09-01 12:59:54 +0530

    Delete lab9.py

--- lab9.py
+++
@@ -1,8 +0,0 @@
-print("******monthly expenses******")
-expenses=0.0
-while True:
-    value=float(input("Enter your amount"))
-    if value==-1:
-        break
-    expenses=expenses+value
-    print("Total expenses:",expenses)
